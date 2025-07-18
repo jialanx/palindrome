ab1ea62c3c392a3eca4ff03459c3d5a42d9959fc
jialanx <jialanxie@gmail.com>
2025-07-17 21:45:03 -0700

    palindrome logic implemented

--- script.js
+++ script.js
@@ -6,5 +6,21 @@ const regex = /[^a-z0-9]/g;
 button.addEventListener("click", () => {
     text = input.value.toLowerCase().replace(regex, "");
     if (text === "") alert("Please input a value");
-    console.log(text); 
+    checkPalidrome(text);
 }); 
+
+const checkPalidrome = (str) => {
+    let newWord = "";
+    str.split("").forEach((letter) => {
+        newWord = letter + newWord;
+        console.log("new word: " + newWord + " string: " + str);
+    })
+    if (newWord === str) {
+        console.log('palindrome');
+    } else {
+        console.log('not palindrome');
+    }
+}
+
+// "_ is a palindrome"
+//"_ is not a palindrome" 
